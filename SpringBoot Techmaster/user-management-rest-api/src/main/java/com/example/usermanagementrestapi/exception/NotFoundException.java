9e1256204d8b2ca28ac5ca798090cc963e263eff
kieutam102001 <74813090+kieutam102001@users.noreply.github.com>
2023-08-18 08:54:49 +0700

    Delete Spring Tech

--- SpringBoot Techmaster/user-management-rest-api/src/main/java/com/example/usermanagementrestapi/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.usermanagementrestapi.exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message){
-        super(message);
-    }
-}
